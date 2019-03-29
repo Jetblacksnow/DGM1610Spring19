4ac033842917eabafe9b49456ce0a994249444bc
RIley Gardiner <rileygardiner@gmail.com>
2019-03-28 23:42:48 -0600

    messing around with some scripted prefabs

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Player;
+    public float moveSpeed;
+    public GameObject Platform;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent=transform;
+        } 
+    }
+   
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject==Player)
+        {
+            Player.transform.parent=null;
+        }
+    }
+
+    private void Start() 
+    
+    {
+        Platform = GetComponet<GameObject>(this.GameObject);
+
+    }
+        void update( )
+        {
+
+
+
+
+
+        }
+
+
+
+}
